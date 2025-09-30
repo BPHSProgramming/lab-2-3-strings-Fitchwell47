a0b47e150eca801ea14eaae2d5290d9488b89ceb
Fitchwell47 <jakefitchwell@gmail.com>
2025-09-30 13:41:41 -0400

    Add files via upload

---
+++ Lab 2.3 Fitchwell.py
@@ -0,0 +1,8 @@
+print("Task #1")
+
+full_name = input("What is your first and last name? ")
+
+first_name = full_name[0:5]
+last_name = full_name[6:15]
+
+print(first_name.title(), last_name.title())
