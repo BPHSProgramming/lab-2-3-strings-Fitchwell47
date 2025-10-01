92d721325a8be3c372bc1efba45ea6abbfe2d052
Fitchwell47 <jakefitchwell@gmail.com>
2025-10-01 13:40:45 -0400

    Add files via upload

--- Lab 2.3 Fitchwell.py
+++ Lab 2.3 Fitchwell.py
@@ -1,9 +1,15 @@
+#Jacob Fitchwell
+#Period 8
+#10/1/25
+
 print("Task #1")
 
 full_name = input("What is your first and last name? ")
 
-#this is hard coded,  What if you use a differen first and last name. How do you find the blank space?
-first_name = full_name[0:5]
-last_name = full_name[6:15]
+space = full_name.find(" ")
+first_name = full_name[0:space]
+last_name = full_name[space: len(full_name)]
 
 print(first_name.title(), last_name.title())
+
+print(first_name[0].lower() + first_name[1:].upper(), last_name[0].lower() + last_name[1:].upper())
