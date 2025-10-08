ee8cea60cea44604ac52aa304aa9257d11b38fe8
jamesgais <gais.james@bphawks.org>
2025-10-08 13:34:02 -0400

    graded

--- Lab 2.3 Fitchwell.py
+++ Lab 2.3 Fitchwell.py
@@ -1,6 +1,8 @@
 #Jacob Fitchwell
 #Period 8
 #10/1/25
+#Overall a nice job. 
+#15/19
 
 print("Task #1")
 
@@ -19,6 +21,8 @@ print("Task #2")
 phrase = 'Once you start down the dark path, forever will it dominate your destiny.'
 
 print(phrase.upper())
+
+#missed the first capital 'o'  REason.. your phrase.upper did not save so you did replace on a mixed case string.
 
 phrase2 = phrase.replace("a","1").replace("e","2").replace("i","3").replace("o","4").replace("u","5")
 
@@ -42,6 +46,7 @@ print("Task #4")
 
 five_numbers = input("Please enter a 5 digit number?")
 
+#conver to int in one step as you slice the number out... much more efficient.
 num_str1 = five_numbers[0]
 num_str2 = five_numbers[1]
 num_str3 = five_numbers[2]
@@ -54,6 +59,7 @@ num_int3 = int(num_str3)
 num_int4 = int(num_str4)
 num_int5 = int(num_str5)
 
+#try not to create this extra variable.
 sum = num_int1 + num_int2 + num_int3 + num_int4 + num_int5
 
 print(num_str1, "+", num_str2, "+", num_str3, "+", num_str4, "+", num_str5, "=", sum)
@@ -76,6 +82,8 @@ today = today.strftime("%Y,%B,%d")
 
 
 print(f"The date today is {today}")
+
+#this is hard coded.  what if the format of hte date changes?  use the find feature tolocate your breaks.
 
 month = today[5:12]
 day = today[13:15]
